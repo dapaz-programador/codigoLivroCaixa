7eb577c7bc280edc3fff5a8799aa4af88006dd37
dapaz-programador <hermesdapaz.programador@gmail.com>
2024-04-03 17:53:00 -0300

    tela de login
    finalizando tala de login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 import {View, Text}from 'react-native';
 
-import{ Background, Container, Logo, AreaInput,Input } from './styles';
+import{ 
+  Background,
+  Container,
+  Logo,
+  AreaInput, 
+  Input, 
+  SubmitButton, 
+  SubmitText,
+  Link,
+  LinkText,
+
+ } from './styles';
+
+//import { Link } from "@react-navigation/native";
 
 export default function SignIn(){
     return(
@@ -17,6 +30,21 @@ export default function SignIn(){
                  placeholder="Seu email"
                />
             </AreaInput>
+
+            <AreaInput>
+               <Input
+                 placeholder="Sua senha"
+               />
+            </AreaInput>
+
+            <SubmitButton>
+              <SubmitText>Acessar</SubmitText>
+            </SubmitButton>
+
+            <Link>
+            <LinkText> Criar uma conta!</LinkText>
+            </Link>
+
           </Container>
 
        </Background>
